47fa022b4c5a0a0b283435c08d5b808807214b60
dirty-dan01 <guilledaniel_1@hotmail.com>
2024-06-27 13:30:54 -0600

    Image Manipulation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import chain from './assets/rand1.jpg';
+import shells from './assets/rand2.jpg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const  time = new Date();
   const day = time.toLocaleString("en-us", {weekday: "long"});
   const morning = time.getHours() >= 6 && time.getHours() <= 12;
   let dayMessage;
+  const [over, setOver] = useState(false);
+  const randonImgURL = "https://picsum.photos/400/265"
 
   if (day.toLowerCase() === "monday") {
     dayMessage = `Happy ${day}`;
@@ -25,6 +29,32 @@ function App() {
     <div className="App">
       <h1>{dayMessage}</h1>
       {morning? <h2>Breakfast time</h2>: ""}
+      <div>
+        <img 
+          onMouseOver={()=>setOver(true)}
+          onMouseOut={()=>setOver(false)}
+          src={over ? shells : chain}
+          alt='imagine there is a random pic here'
+          height={200}
+          width={300}
+        />
+        <img 
+          onMouseOver={()=>setOver(true)}
+          onMouseOut={()=>setOver(false)}
+          src={over ? require("./assets/rand3.jpeg") : require("./assets/rand4.jpeg")}
+          alt='imagine there is another random pic here'
+          height={200}
+          width={300}
+        />
+        <img 
+          onMouseOver={()=>setOver(true)}
+          onMouseOut={()=>setOver(false)}
+          src={over ?  require("./assets/rand5.jpeg") : randonImgURL}
+          alt='imagine there is another random pic here'
+          height={200}
+          width={300}
+        />
+      </div>
     </div>
   );
 }
