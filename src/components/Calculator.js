a361589c7161cdb8665d5ab98f14fb6c712cd3ba
dirty-dan01 <guilledaniel_1@hotmail.com>
2024-07-10 13:20:22 -0600

    Calculator Functional Component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import './Calculator.css'
 
 const Calculator = () => {
+    const inputRef = useRef(null)
+    //const resultRef = useRef(null)
     const[result, setResult] = useState(0)
+    const[inputState, setInputState] = useState("");
+    const handleButtonReset = () => {
+        setResult(0)
+    }
+    function add(e) {
+        e.preventDefault();
+        setResult((result)=> result + Number(inputRef.current.value))
+    }
+
+    function substract(e){
+        e.preventDefault();
+        setResult((result) => result - Number(inputRef.current.value))
+    }
+
+    function multiply(e){
+        e.preventDefault();
+        setResult((result)=> result * Number(inputRef.current.value))
+    }
+
+    function divide(e){
+        e.preventDefault();
+        setResult((result)=> result / Number(inputRef.current.value))
+    }
+
+    function resetInput(e){
+        setInputState(e.target.value)
+    }
   return (
     <div>
-        <h1>Simples working calculator</h1>
+        <h1>Simplest working calculator</h1>
         <h3>{result}</h3>
-        <input/>
+        <input 
+            pattern='[0-9]'
+            ref={inputRef}
+            className='loadNumber'
+            type='number'
+            value={inputState}
+            onChange={resetInput}
+        >
+        
+        </input>
         <div>
-            <button>add</button>
-            <button>substract</button>
-            <button>multiply</button>
-            <button>divide</button>
-            <button>reset input</button>
-            <button>reset result</button>
+            <button onClick={add}>add</button>
+            <button onClick={substract}>substract</button>
+            <button onClick={multiply}>multiply</button>
+            <button onClick={divide}>divide</button>
+            <button onClick={()=>setInputState('')}>reset input</button>
+            <button className='reset-result' onClick={handleButtonReset}>reset result</button>
         </div>
     </div>
   )
